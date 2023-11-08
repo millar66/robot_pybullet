74c5e0f01f9e40e23113b85dd0b0c6554c875e83
lihui.liu <lihui.liu@surgnova.com>
2023-11-08 20:24:51 +0800

    train cylinder

--- push_cylinder_v_point.py
+++ push_cylinder_v_point.py
@@ -19,6 +19,7 @@ import copy
 physicsClient = p.connect(p.GUI)#or p.DIRECT for non-graphical version
 
 #path = [[0.4,0.4,0.3],[0.6,0.6,0.3],[0.8,0.8,0.3],[1.0,1.0,0.3],[1.2,1.2,0.3]]
+# [[0.4,1.0,0.3],[0.6,-1.0,0.3],[0.8,2.0,0.3],[-1.0,0.0,0.3],[-1.2,-2.0,0.3]]
 path_string = input("Please input the path:")
 
 #path = [[0.4,0.0,0.3],[0.6,0.0,0.3],[0.8,0.0,0.3],[1.0,0.0,0.3],[1.2,0.0,0.3]]
@@ -46,6 +47,12 @@ def SetSimulation():
     cylinder_Obstacle_4_StartOrientation = p.getQuaternionFromEuler([0,0,0])
     cylinder_target_StartOrientation = p.getQuaternionFromEuler([0,0,0])
     cylinder_robot_StartOrientation = p.getQuaternionFromEuler([0,0,0])
+    
+    cylinder_Obstacle_text = p.addUserDebugText(text="p1", textPosition=path[0], textColorRGB=[0, 1, 0], textSize=1.2)
+    cylinder_Obstacle_text = p.addUserDebugText(text="p2", textPosition=path[1], textColorRGB=[0, 1, 0], textSize=1.2)
+    cylinder_Obstacle_text = p.addUserDebugText(text="p3", textPosition=path[2], textColorRGB=[0, 1, 0], textSize=1.2)
+    cylinder_Obstacle_text = p.addUserDebugText(text="p4", textPosition=path[3], textColorRGB=[0, 1, 0], textSize=1.2)
+    cylinder_Obstacle_text = p.addUserDebugText(text="p5", textPosition=path[4], textColorRGB=[0, 1, 0], textSize=1.2)
     
     cylinder_Obstacle_1_Id = p.loadURDF("mymodel/cylinder_obstacle1.urdf",cylinder_Obstacle_1_StartPos,cylinder_Obstacle_1_StartOrientation)
     cylinder_Obstacle_2_Id = p.loadURDF("mymodel/cylinder_obstacle2.urdf",cylinder_Obstacle_2_StartPos,cylinder_Obstacle_2_StartOrientation)
@@ -116,8 +123,8 @@ def FindABetterU(Velocity,cost,cylinder_robot_Id,cylinder_target_Id):
         Velocity_temp = copy.deepcopy(Velocity)
         velocity_with_noise.append(Velocity_temp)
         velocity_with_noise[i] = AddNoiseOnU(velocity_with_noise[i])
-    print("Velocity1:",Velocity)
-    print("velocity_with_noise:",velocity_with_noise)
+    # print("Velocity1:",Velocity)
+    # print("velocity_with_noise:",velocity_with_noise)
     #分别计算不同速度时候的cost
     cost_with_noise = []
     for i in range(5):
@@ -132,12 +139,12 @@ def FindABetterU(Velocity,cost,cylinder_robot_Id,cylinder_target_Id):
     #和初始的cost作比较（这里有个if，else的原因是，如果随机到的速度导致的cost非常大，甚至超过了原来的cost，我们可以不更新速度，然后重新给速度一个随机值）
     if cost_with_noise[index]<cost:
         print("cost_with_noise[index]<:",cost_with_noise[index],"cost:",cost)
-        print("Velocity2:",Velocity)
-        print("velocity_with_noise[index]:",velocity_with_noise[index])
+        # print("Velocity2:",Velocity)
+        # print("velocity_with_noise[index]:",velocity_with_noise[index])
         return velocity_with_noise[index],cost_with_noise[index]
     else:
-        print("cost_with_noise[index]>:",cost_with_noise[index],"cost:",cost)
-        print("Velocity2:",Velocity)
+        # print("cost_with_noise[index]>:",cost_with_noise[index],"cost:",cost)
+        # print("Velocity2:",Velocity)
         print("velocity_with_noise[index]:",velocity_with_noise[index])
         return Velocity,cost
 
@@ -160,11 +167,6 @@ print("cost:",cost)
 p.resetSimulation()
 cylinder_robot_Id,cylinder_target_Id = SetSimulation()
 
-cylinder_Obstacle_text = p.addUserDebugText(text="p1", textPosition=path[0], textColorRGB=[0, 1, 0], textSize=1.2)
-cylinder_Obstacle_text = p.addUserDebugText(text="p2", textPosition=path[1], textColorRGB=[0, 1, 0], textSize=1.2)
-cylinder_Obstacle_text = p.addUserDebugText(text="p3", textPosition=path[2], textColorRGB=[0, 1, 0], textSize=1.2)
-cylinder_Obstacle_text = p.addUserDebugText(text="p4", textPosition=path[3], textColorRGB=[0, 1, 0], textSize=1.2)
-cylinder_Obstacle_text = p.addUserDebugText(text="p5", textPosition=path[4], textColorRGB=[0, 1, 0], textSize=1.2)
 while cost > 0.1:
 
 
@@ -176,6 +178,8 @@ while cost > 0.1:
     
     p.resetSimulation()
     cylinder_robot_Id,cylinder_target_Id = SetSimulation()
+    
+cost = RollOut(Velocity,cylinder_robot_Id,cylinder_target_Id,path)
 
 # %%
 p.disconnect()#有连接就有断开
@@ -184,47 +188,45 @@ p.disconnect()#有连接就有断开
 
 
 # %%
-getVisualShapeData
-您可以使用getVisualShapeData访问视觉形状信息。 您可以使用它将您自己的呈现方法与PyBullet模拟连接起来，并在每个模拟步骤之后手动同步世界转换。 您还可以使用GET网格数据，特别是对于可变形对象，来接收有关顶点位置的数据。
-输入参数：
+# getVisualShapeData
+# 您可以使用getVisualShapeData访问视觉形状信息。 您可以使用它将您自己的呈现方法与PyBullet模拟连接起来，并在每个模拟步骤之后手动同步世界转换。 您还可以使用GET网格数据，特别是对于可变形对象，来接收有关顶点位置的数据。
+# 输入参数：
 
-必需/可选参数	参数名字	参数类型	介绍
-必需	objectUniqueId	int	对象的唯一id，由加载方法返回。
-可选	flags	int	VISUAL_SHAPE_DATA_TEXTURE_UNIQUE_IDS还将提供纹理独特的ID。
-可选	physicsClientId	int	物理客户端id由“连接”返回。
-输出参数：
+# 必需/可选参数	参数名字	参数类型	介绍
+# 必需	objectUniqueId	int	对象的唯一id，由加载方法返回。
+# 可选	flags	int	VISUAL_SHAPE_DATA_TEXTURE_UNIQUE_IDS还将提供纹理独特的ID。
+# 可选	physicsClientId	int	物理客户端id由“连接”返回。
+# 输出参数：
 
-参数名字	参数类型	介绍
-objectUniqueId	int	对象的唯一id，由加载方法返回。
-linkIndex	int	基础的链接索引或-1。
-visualGeometryType	int	视觉几何类型(TBD)。
-dimensions	vec3, list of 3 floats	几何尺寸(尺寸，局部尺度）。
-meshAssetFileName	string, list of chars	到三角形网格的路径，如果有的话。 通常相对于URDF、SDF或MJCF文件位置，但可能是绝对的。
-localVisualFrame position	vec3, list of 3 floats	局部视觉框架的位置，相对于链接/关节框架。
-localVisualFrame orientation	vec4, list of 4 floats	局部视觉框架相对于链路/关节框架的方向。
-rgbaColor	vec4, list of 4 floats	红色/绿色/蓝色/阿尔法的URDF颜色(如果指定的话）。
-textureUniqueId	int	(字段只存在于使用VISUAL_SHAPE_DATA_TEXTURE_UNIQUE_IDS标志时)纹理形状的唯一id，或-1如果没有。
+# 参数名字	参数类型	介绍
+# objectUniqueId	int	对象的唯一id，由加载方法返回。
+# linkIndex	int	基础的链接索引或-1。
+# visualGeometryType	int	视觉几何类型(TBD)。
+# dimensions	vec3, list of 3 floats	几何尺寸(尺寸，局部尺度）。
+# meshAssetFileName	string, list of chars	到三角形网格的路径，如果有的话。 通常相对于URDF、SDF或MJCF文件位置，但可能是绝对的。
+# localVisualFrame position	vec3, list of 3 floats	局部视觉框架的位置，相对于链接/关节框架。
+# localVisualFrame orientation	vec4, list of 4 floats	局部视觉框架相对于链路/关节框架的方向。
+# rgbaColor	vec4, list of 4 floats	红色/绿色/蓝色/阿尔法的URDF颜色(如果指定的话）。
+# textureUniqueId	int	(字段只存在于使用VISUAL_SHAPE_DATA_TEXTURE_UNIQUE_IDS标志时)纹理形状的唯一id，或-1如果没有。
 
-changeDynamics
-您可以使用changeDynamics改变质量、摩擦和归位系数等属性。
+# changeDynamics
+# 您可以使用changeDynamics改变质量、摩擦和归位系数等属性。
 
-必需/可选参数	参数名字	参数类型	介绍
-必需	objectUniqueId	int	对象的唯一id，由加载方法返回。
-必需	linkIndex	int	链接指数或-1为基准。
-可选	mass	double	改变链接的质量(或链接指数-1的基础)。
-可选	lateralFriction	double	横向（线性）接触摩擦。
-可选	spinningFriction	double	围绕接触法线的扭转摩擦。
-可选	rollingFriction	double	与接触法线正交的扭转摩擦力（保持这个值非常接近于零，否则模拟会变得非常不真实）。
+# 必需/可选参数	参数名字	参数类型	介绍
+# 必需	objectUniqueId	int	对象的唯一id，由加载方法返回。
+# 必需	linkIndex	int	链接指数或-1为基准。
+# 可选	mass	double	改变链接的质量(或链接指数-1的基础)。
+# 可选	lateralFriction	double	横向（线性）接触摩擦。
+# 可选	spinningFriction	double	围绕接触法线的扭转摩擦。
+# 可选	rollingFriction	double	与接触法线正交的扭转摩擦力（保持这个值非常接近于零，否则模拟会变得非常不真实）。
 
-resetBaseVelocity
-您可以使用重置基本速度重置身体底部的线性和/或角速度
-输入参数：
+# resetBaseVelocity
+# 您可以使用重置基本速度重置身体底部的线性和/或角速度
+# 输入参数：
 
-必需/可选参数	参数名字	参数类型	介绍
-必需	objectUniqueId	int	对象的唯一id，由加载方法返回。
-可选	linearVelocity	vec3, list of 3 floats	直角世界坐标中的线速度[x，y，z]。
-可选	angularVelocity	vec3, list of 3 floats	直角速度[wx，wy，wz]在笛卡尔世界坐标中。
-可选	physicsClientId	int	如果您连接到多个服务器，您可以选择一个。
-————————————————
-版权声明：本文为CSDN博主「墨绿色的摆渡人」的原创文章，遵循CC 4.0 BY-SA版权协议，转载请附上原文出处链接及本声明。
-原文链接：https://blog.csdn.net/xzs1210652636/article/details/109234531
+# 必需/可选参数	参数名字	参数类型	介绍
+# 必需	objectUniqueId	int	对象的唯一id，由加载方法返回。
+# 可选	linearVelocity	vec3, list of 3 floats	直角世界坐标中的线速度[x，y，z]。
+# 可选	angularVelocity	vec3, list of 3 floats	直角速度[wx，wy，wz]在笛卡尔世界坐标中。
+# 可选	physicsClientId	int	如果您连接到多个服务器，您可以选择一个。
+
